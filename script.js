6d75bd46d0dea345b965dab8168c18af6993fe39
KaizerBalite <146916590+KaizerBalite@users.noreply.github.com>
2023-10-04 14:35:32 +0800

    feat: Activity 1 in DSA
    Arithmetic Operators

---
+++ script.js
@@ -0,0 +1,12 @@
+//Activity 1
+
+let firstFavNum = 9;
+let secondFavNum = 0;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(Math.pow(firstFavNum));
+
